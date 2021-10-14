22f45d14b50a3ab6751d01064206d7e1e7450ca6
javierpb275 <javierpb275@gmail.com>
2021-10-14 17:05:40 +0200

    conditional logic: if, elif and else

---
+++ conditional_logic.py
@@ -0,0 +1,11 @@
+is_old = True
+is_licenced = False
+
+if is_old and is_licenced:
+    print('you can drive now')
+elif is_old:
+    print('you are old enough to drive but you do not have a licence!')
+else:
+    print('you are not of age!')
+
+print('okok')
