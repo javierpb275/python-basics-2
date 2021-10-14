ae8cdfe9ff8e5c702035718b4662d1dd60750fdb
javierpb275 <javierpb275@gmail.com>
2021-10-14 17:43:52 +0200

    ternary operator

--- conditional_logic.py
+++ conditional_logic.py
@@ -18,3 +18,11 @@ bigger_than_0 = 5  # True
 empty_string = ""  # False
 cero = 0  # False
 null = None  # False
+
+
+#ternary operator
+
+#condition_if_true if condition else condition_if_false
+is_friend = True
+can_message = "message allowed" if is_friend else "not allowed to message"
+print(can_message)#message allowed
