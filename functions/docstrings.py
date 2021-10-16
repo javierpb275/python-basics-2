b150f3bfddc69a767c0ed0a1890d1148d018f60f
javierpb275 <javierpb275@gmail.com>
2021-10-16 17:37:21 +0200

    docstrings

---
+++ functions/docstrings.py
@@ -0,0 +1,18 @@
+def print_sth(sth):
+    '''
+    Info: this function prints the argument that we pass
+    '''
+    print(sth)
+
+
+help(print_sth)
+
+'''
+Help on function print_sth in module __main__:
+
+print_sth(sth)
+    Info: this function prints the argument that we pass
+
+'''
+
+print(print_sth.__doc__)  # Info: this function prints the argument that we pass
