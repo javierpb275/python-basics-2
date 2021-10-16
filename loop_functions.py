05f580a414d7e717d4cb0b7ee586ac4d50918e93
javierpb275 <javierpb275@gmail.com>
2021-10-16 12:38:31 +0200

    enumerate()

--- loop_functions.py
+++ loop_functions.py
@@ -14,3 +14,22 @@ for _ in range(10, 0, -1):
     print(_)  # 10, 9, 8... 1
 
 print(list(range(10)))  # [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+
+# enumerate(iterable: Iterable, start: int = ...)
+for char in enumerate('Hellooo'):
+    print(char)  # (0, 'H'), (1, 'e')... (6, 'o')
+
+for i, char in enumerate('Hellooo'):
+    print(i, char)  # 0 H, 1 e... 6 o
+
+for i, char in enumerate([1, 2, 3]):
+    print(i, char)  # 0 1, 1 2, 2 3
+
+for i, char in enumerate((1, 2, 3)):
+    print(i, char)  # 0 1, 1 2, 2 3
+
+# exercise:
+for i, char in enumerate(list(range(20))):
+    if char == 15:
+        print(f'index of 15 is: {i}')
